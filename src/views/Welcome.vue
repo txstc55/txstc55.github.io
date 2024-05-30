24c0c54b1ae85e5d2ea0afd88b42fb0b33269787
txstc55 <txstc55@gmail.com>
2024-05-29 21:53:08 -0700

    add info

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="w-screen h-screen flex items-center justify-center text-center blank text-8xl text-white" :style="{'font-family':'Asap'}">
-    You Found Me<br>I'm Xuan Tang
+  <div
+    class="w-screen h-screen flex items-center justify-center text-center blank text-8xl text-white select-none relative"
+    :style="{ 'font-family': 'Asap' }"
+  >
+    You Found Me<br />I'm Xuan Tang
+    <div
+      class="absolute bottom-10 text-white text-center w-full select-none text-sm"
+      :style="{ 'font-family': 'Asap' }"
+    >
+      This site is still under construction
+    </div>
   </div>
 </template>
-
 
 <script>
 export default {
@@ -23,7 +31,7 @@ export default {
 </script>
 
 <style scoped>
-.blank{
+.blank {
   background-color: black;
 }
 </style>
